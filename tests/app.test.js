2d724c7b29d17f37c1a84eb41c15dacb26af0ca8
MARIANA MENDES E SILVA 115211452 <marianams@lcc3-095.lcc.ufcg.edu.br>
2018-09-05 16:23:48 -0300

    morgan

---
+++ tests/app.test.js
@@ -0,0 +1,11 @@
+const app = require('../index'); // ou server... ou app.
+const request = require('supertest');
+
+request(app)
+  .get('/user')
+  .expect('Content-Type', /json/)
+  .expect('Content-Length', '15')
+  .expect(200)
+  .end(function(err, res) {
+    if (err) throw err;
+  });
