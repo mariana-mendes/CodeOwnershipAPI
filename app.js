407d2c0153a6031591c96d3416c4cec2dea39d63
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2018-10-07 13:39:03 -0300

    nada funciona, feijoada

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const router = express.Router();
 const cors = require('cors')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const passport = require('passport'),  LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
 
 mongoose.connect('mongodb://localhost/test');
 
@@ -29,91 +26,7 @@ app.use('/class', classRoute);
 app.use('/author', authorRoute);
 app.use('/project', projectRoute);
 
-app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
-app.use(passport.initialize())
-app.use(passport.session())
 
 
-passport.use(new LocalStrategy(
-      { usernameField: 'email' },
-      (email, password, done) => {
-        axios.get(`http://localhost:5000/users?email=${email}`)
-        .then(res => {
-          const user = res.data[0]
-          if (!user) {
-            return done(null, false, { message: 'Invalid credentials.\n' });
-          }
-          if (password != user.password) {
-            return done(null, false, { message: 'Invalid credentials.\n' });
-          }
-          return done(null, user);
-        })
-        .catch(error => done(error));
-      }
-));
-    
-    // tell passport how to serialize the user
-    passport.serializeUser((user, done) => {
-      done(null, user.id);
-    });
-    
-    passport.deserializeUser((id, done) => {
-      axios.get(`http://localhost:5000/users/${id}`)
-      .then(res => done(null, res.data) )
-      .catch(error => done(error, false))
-    });
-
-app.use(session({
-      genid: (req) => {
-        console.log('Inside session middleware genid function')
-        console.log(`Request object sessionID from client: ${req.sessionID}`)
-        return uuid() // use UUIDs for session IDs
-      },
-      store: new FileStore(),
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: true
-    }))
-    app.use(passport.initialize());
-    app.use(passport.session());
-    
-    // create the homepage route at '/'
-    app.get('/', (req, res) => {
-      console.log('Inside the homepage callback')
-      console.log(req.sessionID)
-      res.send(`You got home page!\n`)
-    })
-    
-    // create the login get and post routes
-    app.get('/login', (req, res) => {
-      console.log('Inside GET /login callback')
-      console.log(req.sessionID)
-      res.send(`You got the login page!\n`)
-    })
-    
-    app.post('/login', (req, res, next) => {
-      console.log('Inside POST /login callback')
-      passport.authenticate('local', (err, user, info) => {
-        console.log('Inside passport.authenticate() callback');
-        console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
-        console.log(`req.user: ${JSON.stringify(req.user)}`)
-        req.login(user, (err) => {
-          console.log('Inside req.login() callback')
-          console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
-          console.log(`req.user: ${JSON.stringify(req.user)}`)
-          return res.send('You were authenticated & logged in!\n');
-        })
-      })(req, res, next);
-    })
-    
-    app.get('/authrequired', (req, res) => {
-      console.log('Inside GET /authrequired callback')
-      console.log(`User authenticated? ${req.isAuthenticated()}`)
-      if(req.isAuthenticated()) {
-        res.send('you hit the authentication endpoint\n')
-      } else {
-        res.redirect('/')
-      }
-    })
 module.exports = app;
 
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const app = express();
+const User = require('../user/user')
+const passport = require('passport')
+
+
+router.use(passport.initialize())
     
+router.get('/', (req, res, next) => 
+        res.status(200).send({ app: "running"})
+    );
+
+
+router.get('/register', (req, res, next) => 
+///validar 
+    res.status(200).send({ app: "register view"})
+);
+
+router.post('/register', (req, res, next) => {
+    const newUser = new User(req.body);
+    newUser.save(function (err, newUser) {
+      if (err) return console.error(err);
+    });
+
+    req.login(newUser._id, function(){
+        res.redirect('/');
+    })
+});
 module.exports = router;
