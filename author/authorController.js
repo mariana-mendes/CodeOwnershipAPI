ee8150f498c3e55b51e873ac04b4a147ff7e09c1
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2018-10-09 20:01:04 -0300

    ajeitando as rotas

--- author/authorController.js
+++ author/authorController.js
@@ -1,16 +1,16 @@
 const Author = require('./author')
 
 exports.get = (req, res, next) => {
-    Author.find(function (err, kittens) {
+    Author.find(function (err, authors) {
         if (err) return console.error(err);
-        console.log(kittens);
+        console.log(authors);
     })
     res.status(200).send('uu');};
 
 exports.getAll = (req, res, next) => {
-        Author.find(function (err, kittens) {
+        Author.find(function (err, author) {
             if (err) return console.error(err);
-            console.log(kittens);
+            console.log(author);
         })
         res.status(200).send('uu');};
 
@@ -19,8 +19,7 @@ exports.post = (req, res, next) =>{
     newAuthor.save(function (err, newAuthor) {
       if (err) return console.error(err);
     });
-    console.log(newAuthor)
-    res.status(201).send('new')
+    res.status(201).send(newAuthor)
 }
 exports.put = (req, res, next) =>  res.status(201).send('authorPut')
 exports.delete = (req, res, next) => res.status(200).send('authorDelete')
